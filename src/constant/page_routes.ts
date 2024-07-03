119cd5549717b89437907dad6c3818ca8c5e2490
rafmasloman <raflymasloman20@gmail.com>
2024-07-04 00:20:50 +0800

    fix: add more page  for page routes

--- src/constant/page_routes.ts
+++ src/constant/page_routes.ts
@@ -4,16 +4,18 @@ export const HOMEPAGE = '/';
 export const CONTACT_US = '/contact-us';
 
 // ADMIN ROUTES
-export const DASHBOARD_ADMIN_PAGE = '/dashboard';
-export const CLIENT_ADMIN_PAGE = '/client';
+export const DASHBOARD_ADMIN_PAGE = '/admin/dashboard';
+export const CLIENT_ADMIN_PAGE = '/admin/client';
 export const USER_ADMIN_PAGE = '/admin/user-management';
-export const MEMBER_ADMIN_PAGE = '/member';
-export const PROJECTS_ADMIN_PAGE = '/project';
+export const MEMBER_ADMIN_PAGE = '/admin/member';
+export const PROJECTS_ADMIN_PAGE = '/admin/project';
 
 // PROJECT MANAGER ROUTES
 export const TASK_PM_PAGE = '/task';
 export const MILESTONE_PM_PAGE = '/milestone';
 export const PAYROLL_PM_PAGE = '/payroll';
+export const PROJECT_PM_PAGE = '/project-manager/project';
+export const PROJECT_DETAIL_PM_PAGE = '/project-manager/project';
 
 // MEMBER ROUTES
 export const PROFILE = '/profile';
