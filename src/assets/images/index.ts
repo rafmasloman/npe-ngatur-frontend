2d370e328d8c9553f0581639384c081024e6ec58
rafmasloman <raflymasloman20@gmail.com>
2024-07-03 15:04:34 +0800

    feat: add images and export images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,15 @@
+import AppDefault01 from '../images/default-project.png';
+import AppDefault02 from '../images/default-project-detail.png';
+import ThumbnailImage from '../images/main_thumbnail.png';
+import AbstractImage from '../illustration/abstract.png';
+import NPENgaturLogo from '../images/npe_ngatur.png';
+import NPENgaturWhiteLogo from '../images/npe_ngatur_white.png';
+
+export {
+  AppDefault01,
+  AppDefault02,
+  ThumbnailImage,
+  AbstractImage,
+  NPENgaturLogo,
+  NPENgaturWhiteLogo,
+};
