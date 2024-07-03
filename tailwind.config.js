c00693b816202cdd1f2cc6674a8f8317190d6c25
rafmasloman <raflymasloman20@gmail.com>
2024-07-03 14:57:51 +0800

    feat: install more packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: require('./src/constant/colors').COLORS,
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+      },
+    },
   },
   plugins: [],
 };
