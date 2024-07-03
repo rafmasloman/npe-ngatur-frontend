519780c369cef146ca73c240bca849573149c58b
rafmasloman <raflymasloman20@gmail.com>
2024-07-03 15:13:13 +0800

    feat : create layouts for workspace member

---
+++ src/app/project-manager/layout.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import {
+  AppShell,
+  Container,
+  Group,
+  Image,
+  Menu,
+  Stack,
+  Text,
+} from '@mantine/core';
+import { ReactNode } from 'react';
+import NPELogoWhite from '../../assets/images/npe_ngatur.png';
+import { IoWallet } from 'react-icons/io5';
+import { GoHomeFill } from 'react-icons/go';
+import { BiSolidDashboard } from 'react-icons/bi';
+import { FaFolderOpen, FaPerson } from 'react-icons/fa6';
+
+import NavbarItems from '../../components/Navbar/NavbarItems';
+import UserNavigationMenu from '../../components/Menu/UserNavigationMenu';
+import HeaderLayout from '../../layouts/HeaderLayout';
+
+interface IProjectManagerLayoutPropsType {
+  children: ReactNode;
+}
+
+const ProjectManagerLayout = ({ children }: IProjectManagerLayoutPropsType) => {
+  return (
+    <AppShell
+      header={{ height: 100 }}
+      classNames={{
+        main: `bg-custom_black `,
+        header: `bg-custom_black border-0`,
+      }}
+    >
+      <AppShell.Header>
+        <HeaderLayout />
+      </AppShell.Header>
+
+      <AppShell.Main>
+        <Container
+          ff={'poppins'}
+          className="bg-neutral-50 h-screen rounded-tr-[50px] rounded-tl-[50px] p-10"
+          fluid
+        >
+          {children}
+        </Container>
+      </AppShell.Main>
+    </AppShell>
+  );
+};
+
+export default ProjectManagerLayout;
