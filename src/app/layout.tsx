c79c6605cca7a72e40e871a3c98821215f4e0c45
rafmasloman <raflymasloman20@gmail.com>
2024-07-03 22:06:31 +0800

    feat: change website logo to npe ngatur logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,21 +5,17 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../../theme';
 import ReactQueryProvider from '../libs/providers/react-query-provider';
 
-export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
-};
+// export const metadata = {
+//   title: 'Mantine Next.js template',
+//   description: 'I am using Mantine with Next.js!',
+// };
 
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
+        <link rel="shortcut icon" href="/npe_logo.svg" />
       </head>
       <body>
         <ReactQueryProvider>
