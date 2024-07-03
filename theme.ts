9f0ad820e9a785bfc6c7b7fd5ee176670bd40a4d
rafmasloman <raflymasloman20@gmail.com>
2024-07-03 14:58:44 +0800

    feat: setup mantine theme configuration

--- theme.ts
+++ theme.ts
@@ -1,7 +1,41 @@
-"use client";
+'use client';
 
-import { createTheme } from "@mantine/core";
+import { createTheme, DEFAULT_THEME } from '@mantine/core';
+import { COLORS } from './src/constant/colors';
+import { poppins } from './src/libs/next-font';
+
+type ColorsShadeTuple = [
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+  string,
+];
+
+function getCustomColors(colors: { [key: string]: string }): {
+  [key: string]: ColorsShadeTuple;
+} {
+  const finalColors: { [key: string]: ColorsShadeTuple } = {};
+  const colorsName = Object.keys(colors);
+
+  for (let i = 0; i < colorsName.length; i++) {
+    finalColors[`${colorsName[i]}`] = new Array(10).fill(
+      colors[colorsName[i]],
+    ) as ColorsShadeTuple;
+  }
+
+  return finalColors;
+}
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  colors: {
+    ...getCustomColors(COLORS),
+  },
+  fontFamily: poppins.className,
 });
