56033edd962b868b42fdf876ca04b8f0a2e8aed9
rafmasloman <raflymasloman20@gmail.com>
2024-07-04 00:21:12 +0800

    fix: change height to min h screen

--- src/app/project-manager/layout.tsx
+++ src/app/project-manager/layout.tsx
@@ -30,7 +30,7 @@ const ProjectManagerLayout = ({ children }: IProjectManagerLayoutPropsType) => {
     <AppShell
       header={{ height: 100 }}
       classNames={{
-        main: `bg-custom_black `,
+        main: `bg-custom_black`,
         header: `bg-custom_black border-0`,
       }}
     >
@@ -41,7 +41,7 @@ const ProjectManagerLayout = ({ children }: IProjectManagerLayoutPropsType) => {
       <AppShell.Main>
         <Container
           ff={'poppins'}
-          className="bg-neutral-50 rounded-tr-[36px] rounded-tl-[36px] py-10 px-2 md:p-10"
+          className="bg-neutral-50 min-h-screen rounded-tr-[36px] rounded-tl-[36px] py-10 px-2 md:p-10"
           fluid
         >
           {children}
