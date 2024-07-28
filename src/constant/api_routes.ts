9bf1ae9a885f2d97f0e7aa36594ebc1398bcb6b7
rafmasloman <raflymasloman20@gmail.com>
2024-07-28 13:32:58 +0800

    feat : add constant api url for middleware and auth

--- src/constant/api_routes.ts
+++ src/constant/api_routes.ts
@@ -10,4 +10,10 @@ export const API_ROUTES = {
   INVOICE: 'invoice',
   PAYROLL: 'payroll',
   PROFILE: 'profile',
+  LOGIN: 'auth/login',
+  AUTH_CREDENTIAL: 'auth/credential',
 };
+
+export const API_BASE_URL = 'http://localhost:5000/api/v1';
+export const API_MIDDLEWARE_BASE_DEVELOPMENT_URL =
+  'http://103.127.134.2:7916/api/v1';
