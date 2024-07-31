b8deb02698e631970379f7b5302e92026091544a
rafmasloman <raflymasloman20@gmail.com>
2024-08-01 02:05:27 +0800

    feat : add hooks to get payroll member

--- src/services/payroll/hooks/useQueryPayrollMember.ts
+++ src/services/payroll/hooks/useQueryPayrollMember.ts
@@ -2,10 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { GET_PAYROLL_MEMBER } from '../../../constant/query_key';
 import payrollService from '../PayrollService';
 
-const useQueryPayrollMember = (payrollId?: string) => {
+const useQueryPayrollMember = (userId?: string) => {
   const query = useQuery({
-    queryKey: [GET_PAYROLL_MEMBER, payrollId],
-    queryFn: () => payrollService.getPayrollMember(payrollId),
+    queryKey: [GET_PAYROLL_MEMBER, userId],
+    queryFn: () => payrollService.getPayrollMember(userId),
     select(data) {
       return data.data;
     },
