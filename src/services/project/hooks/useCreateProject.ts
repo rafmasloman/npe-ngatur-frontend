2e1ff5766dede9696d16d985ef0c69e3f90f1356
rafmasloman <raflymasloman20@gmail.com>
2024-08-09 21:58:15 +0800

    fix: change word user to project on create project hooks

--- src/services/project/hooks/useCreateProject.ts
+++ src/services/project/hooks/useCreateProject.ts
@@ -1,6 +1,14 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { USER_ADMIN_PAGE } from '../../../constant/page_routes';
-import { CREATE_USER, GET_ALL_USERS } from '../../../constant/query_key';
+import {
+  PROJECTS_ADMIN_PAGE,
+  USER_ADMIN_PAGE,
+} from '../../../constant/page_routes';
+import {
+  CREATE_PROJECT,
+  CREATE_USER,
+  GET_ALL_PROJECTS,
+  GET_ALL_USERS,
+} from '../../../constant/query_key';
 import { useRouter } from 'next/navigation';
 import NotificationAdmin from '../../../features/common/components/NotificationAdmin';
 import projectService from '../ProjectsService';
@@ -10,7 +18,7 @@ const useCreateProject = () => {
   const router = useRouter();
 
   const query = useMutation({
-    mutationKey: [CREATE_USER],
+    mutationKey: [CREATE_PROJECT],
     mutationFn: projectService.createProject,
     onSuccess(data, variables, context) {
       if (!data.data) {
@@ -21,19 +29,19 @@ const useCreateProject = () => {
         });
       } else {
         NotificationAdmin({
-          title: 'User ditambah',
-          message: 'Berhasil menambah data user',
+          title: 'Project ditambah',
+          message: 'Berhasil menambah data Project',
           status: 'SUCCESS',
         });
-        queryClient.invalidateQueries({ queryKey: [GET_ALL_USERS] });
+        queryClient.invalidateQueries({ queryKey: [GET_ALL_PROJECTS] });
 
-        router.push(USER_ADMIN_PAGE);
+        router.push(PROJECTS_ADMIN_PAGE);
       }
     },
     onError(error, variables, context) {
       NotificationAdmin({
-        title: 'User gagal ditambah',
-        message: 'Gagal menambah data user',
+        title: 'Project gagal ditambah',
+        message: 'Gagal menambah data Project',
         status: 'FAILED',
       });
     },
