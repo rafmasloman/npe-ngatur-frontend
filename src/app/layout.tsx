2c8a03203d7c72246521772cb2282e046e352e81
rafmasloman <raflymasloman20@gmail.com>
2024-08-09 22:10:05 +0800

    feat : add dnd, notification, and authentication provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/charts/styles.css';
+import '@mantine/dates/styles.css';
+
 import './globals.css';
 import React from 'react';
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../../theme';
 import ReactQueryProvider from '../libs/providers/react-query-provider';
+import { Notifications } from '@mantine/notifications';
+import ReactDNDProvider from '../libs/providers/react-dnd-provider';
+import AuthProviders from '../libs/providers/auth-provider';
 
 // export const metadata = {
 //   title: 'Mantine Next.js template',
@@ -17,10 +24,17 @@ export default function RootLayout({ children }: { children: any }) {
         <ColorSchemeScript />
         <link rel="shortcut icon" href="/npe_logo.svg" />
       </head>
-      <body>
-        <ReactQueryProvider>
-          <MantineProvider theme={theme}>{children}</MantineProvider>
-        </ReactQueryProvider>
+      <body className="">
+        <ReactDNDProvider>
+          <MantineProvider theme={theme}>
+            <ReactQueryProvider>
+              <AuthProviders>
+                <Notifications />
+                {children}
+              </AuthProviders>
+            </ReactQueryProvider>
+          </MantineProvider>
+        </ReactDNDProvider>
       </body>
     </html>
   );
