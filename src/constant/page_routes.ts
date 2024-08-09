00f4e000b8ce51ebb68640d24600da16a6fde775
rafmasloman <raflymasloman20@gmail.com>
2024-08-09 21:55:44 +0800

    fix : add more value constant for page and query key

--- src/constant/page_routes.ts
+++ src/constant/page_routes.ts
@@ -32,5 +32,13 @@ export const PROJECT_PM_PAGE = '/project-manager/project';
 export const PROJECT_DETAIL_PM_PAGE = '/project-manager/project';
 export const PROFILE_PM_PAGE = '/project-manager/profile';
 
+// STAFF ROUTES
+export const DASHBOAR_STAFF_PAGE = 'staff';
+export const TASK_STAFF_PAGE = '/staff/task';
+export const MILESTONE_STAFF_PAGE = '/staff/milestone';
+export const PAYROLL_STAFF_PAGE = '/staff/payroll';
+export const PROJECT_STAFF_PAGE = '/staff/project';
+export const PROFILE_STAFF_PAGE = '/staff/profile';
+
 // MEMBER ROUTES
 export const PROFILE = '/profile';
