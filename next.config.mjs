ca8089495e8b0bc6b26a3a5386d2a5f2b647ed71
rafmasloman <raflymasloman20@gmail.com>
2024-08-13 13:41:50 +0800

    build : add external dependencies for this project

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+
+    return config;
+  },
 };
 
 export default nextConfig;
