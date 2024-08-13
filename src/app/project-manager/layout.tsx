1330513d247d21fa3ba7dc2910f61074039c68e1
rafmasloman <raflymasloman20@gmail.com>
2024-08-13 13:40:36 +0800

    feat : create dashboard page for pm

--- src/app/project-manager/layout.tsx
+++ src/app/project-manager/layout.tsx
@@ -1,25 +1,6 @@
 'use client';
 
-import {
-  AppShell,
-  Container,
-  Group,
-  Image,
-  Menu,
-  Stack,
-  Text,
-} from '@mantine/core';
 import { ReactNode } from 'react';
-import NPELogoWhite from '../../assets/images/npe_ngatur.png';
-import { IoWallet } from 'react-icons/io5';
-import { GoHomeFill } from 'react-icons/go';
-import { BiSolidDashboard } from 'react-icons/bi';
-import { FaFolderOpen, FaPerson } from 'react-icons/fa6';
-
-import NavbarItems from '../../components/Navbar/NavbarItems';
-import UserNavigationMenu from '../../components/Menu/UserNavigationMenu';
-import HeaderLayout from '../../layouts/HeaderLayout';
-import SEO from '../../components/SEO/BaseSEO';
 import WorkspaceLayout from '../../layouts/WorkspaceLayout';
 
 interface IProjectManagerLayoutPropsType {
