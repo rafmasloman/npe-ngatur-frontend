687725c74fb8266b9b83c4f5c067488e0398b275
rafmasloman <raflymasloman20@gmail.com>
2024-08-28 22:48:48 +0800

    fix : change package

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
-  // swcMinify: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  // output: 'standalone',
   webpack: (config) => {
     config.resolve.alias.canvas = false;
 
